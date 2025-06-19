8f3d23dd42b2ffafa0f1079271809eaf9ed275a0
John Morris <john@zultron.com>
2025-06-19 11:31:28 -0500

    Fix "save-always" input
    The "save-always" input failed to force "post" script run because
    `github.event.inputs` refers to workflow dispatch or workflow call
    inputs, not the action's `inputs:` defined inside the `action.yml`, so
    `github.event.inputs.save-always` was never true.
    
    Turns out it's hard to get the job's status inside the action as well,
    and there's no equivalent of the `success()` function.  The
    workaround, passing `job.status` in as an input, was cribbed from
    `actions/setup-java`, where the `job-status` input is defined in
    `action.yml` [1], and passed through (in a loopy way) for retrieval
    from a utility function [2].
    
    [1]:  https://github.com/actions/setup-java/blob/f4f1212c88/action.yml#L62
    [2]:  https://github.com/actions/setup-java/blob/f4f1212c88/src/util.ts#L86-L88

--- src/extract-cache.ts
+++ src/extract-cache.ts
@@ -1,3 +1,4 @@
+import getInput from '@actions/core'
 import { promises as fs } from 'fs';
 import path from 'path';
 import {CacheOptions, Opts, getCacheMap, getMountArgsString, getTargetPath, getBuilder} from './opts.js';
@@ -50,6 +51,10 @@ export async function extractCaches(opts: Opts) {
     if (opts["skip-extraction"]) {
         console.log("skip-extraction is set. Skipping extraction step...");
         return;
+    }
+    if (!opts["save-always"] && getInput("job-status") !== "success") {
+      console.log("Skipping extraction:  job.status not 'success'");
+      return;
     }
 
     const cacheMap = await getCacheMap(opts);
